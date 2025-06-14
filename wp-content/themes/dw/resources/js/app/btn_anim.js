ecc1ac032f14de48b33ff831f00fb2baf98bccc0
SamanthaClaes <Samantha.claes@student.hepl.be>
2025-06-14 21:19:51 +0200

    add js

--- wp-content/themes/dw/resources/js/app/btn_anim.js
+++ wp-content/themes/dw/resources/js/app/btn_anim.js
@@ -1,13 +1,3 @@
-console.log('test')
-console.log('test du header')
-
-document.addEventListener('DOMContentLoaded', () => {
-    const header = document.querySelector('header');
-    if (header) {
-        header.classList.add('animate-slide-in');
-    }
-});
-
 document.addEventListener('DOMContentLoaded', () => {
     const button = document.querySelector('.animated-submit');
     if (!button) return;
--- wp-content/themes/dw/resources/js/app/header.js
+++ wp-content/themes/dw/resources/js/app/header.js
@@ -1,1 +1,8 @@
 console.log('test du header')
+
+document.addEventListener('DOMContentLoaded', () => {
+    const header = document.querySelector('header');
+    if (header) {
+        header.classList.add('animate-slide-in');
+    }
+});
